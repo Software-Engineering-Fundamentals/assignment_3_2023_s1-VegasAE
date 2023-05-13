7abfa39f7a2d631f98e6f7ff4dc90fd686e168a5
VegasAE <s3947138@student.rmit.edu.au>
2023-05-13 18:45:40 +1000

    Created skeleton user class

---
+++ Group 42/src/main/java/User.java
@@ -0,0 +1,51 @@
+import java.util.Date;
+
+public class User {
+
+    // Private class attributes
+    protected String name;
+    protected String userID;
+    protected Date DOB;
+    protected String email;
+    protected int phoneNum;
+    protected String password;
+    protected String emergencyContact;
+    private String payment;
+
+
+    // get details of the user
+    public void getDetails(){
+        
+    }
+
+    // set the details of the user TODO: determine how these details are to be collected
+    public void setDetails(){
+
+    }
+
+    // create an enquiry request
+    public void createEnquiry(){
+
+    }
+
+    // create IT support ticket
+    public void createSupportTicket(){
+
+    }
+
+    // allow user to join a course
+    public void joinCourse(){
+
+    }
+
+    // allow user to register themselves with the training service
+    public void register(){
+
+    }
+
+    // allows the user to login
+    public void login(){
+
+    }
+
+}
