474655d321045849e187b4c01adf0a02e4ccc274
VegasAE <s3947138@student.rmit.edu.au>
2023-05-13 18:10:33 +1000

    created Main file

---
+++ Group 42/src/main/java/MainSystem.java
@@ -0,0 +1,10 @@
+/*
+ * This is the main system file from where all classes will be initialised
+ * and methods are called.
+ */
+
+class MainSystem {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
